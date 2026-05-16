7e6ef15c84a92ff15699e435290402665bf44cc6
idcc01 <2189035817@qq.com>
2026-05-17 00:31:30 +0800

    Update appid and appsecret in getcode.php

--- getcode.php
+++ getcode.php
@@ -1,7 +1,7 @@
 <?php
 // --------------------- 你只需要改这 2 个信息 ---------------------
-$appid       = "wx35eec71ceb028e8f";       // 你的公众号APPID
-$appsecret   = "这里改成你的APPSECRET";     // 你的APPSECRET
+$appid       = "wxde2ad02f02cb0df5";       // 你的公众号APPID
+$appsecret   = "f27a1e32177f425fe8c940dc8d063b32";     // 你的APPSECRET
 $target_url  = "https://ipp.noteflow.me/home"; // 最终要去的网站
 // ----------------------------------------------------------------
 
