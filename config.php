8cd60ef4a492f06be658fd90a12a6996ae07de7f
idcc01 <2189035817@qq.com>
2026-05-17 01:55:09 +0800

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// 安全密钥（两端保持一样！！！）
-define('HMAC_KEY', 't68X8N9k65GzA7pQ2sF4vB7nR8tU0wXy');
-
-// 公众号配置
-define('APPID', 'wxde2ad02f02cb0df5');
-define('SECRET', 'f27a1e32177f425fe8c940dc8d063b32'); // 换成你真实密钥
-define('TARGET_URL', 'https://ipp.noteflow.me/home');
-?>
