35efc8313959086a0cbe286b8f33507d6e1026c7
idcc01 <2189035817@qq.com>
2026-05-17 00:38:51 +0800

    Update getcode.php

--- getcode.php
+++ getcode.php
@@ -1,36 +1,15 @@
 <?php
-// --------------------- 你只需要改这 2 个信息 ---------------------
-$appid       = "wxde2ad02f02cb0df5";       // 你的公众号APPID
-$appsecret   = "f27a1e32177f425fe8c940dc8d063b32";     // 你的APPSECRET
-$target_url  = "https://ipp.noteflow.me/home"; // 最终要去的网站
-// ----------------------------------------------------------------
+// 目标网站
+$target = "https://ipp.noteflow.me/home";
 
-// 1. 获取微信返回的 code 和 state
-$code  = $_GET['code'] ?? '';
-$state = $_GET['state'] ?? '';
+// 获取所有推广参数
+$query = $_SERVER['QUERY_STRING'];
 
-if (empty($code)) {
-    header("Location: $target_url");
-    exit;
+// 直接跳转！不做任何微信登录操作！
+if (!empty($query)) {
+    header("Location: $target?$query");
+} else {
+    header("Location: $target");
 }
-
-// 2. 用 code 换取 openid
-$url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=$appid&secret=$appsecret&code=$code&grant_type=authorization_code";
-$res = file_get_contents($url);
-$arr = json_decode($res, true);
-
-$openid = $arr['openid'] ?? '';
-
-// 3. 拼接参数，跳转到你的最终网站
-$params = http_build_query([
-    'code'    => $code,
-    'openid'  => $openid,
-    'state'   => $state
-]);
-
-$go = $target_url . '?' . $params;
-
-// 4. 自动跳转（带所有参数）
-header("Location: $go");
 exit;
 ?>
